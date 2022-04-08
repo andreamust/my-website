e4156830bc6af8053e81b5a8ea26638cee277495
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2022-04-09 01:26:40 +0200

    bug fixing

--- components/ui/windows/basic-window.js
+++ components/ui/windows/basic-window.js
@@ -2,11 +2,11 @@ import { motion } from 'framer-motion';
 import WindowTopBar from './window-topbar';
 
 function BasicWindow(props) {
+  const width = `w-[${props.width || '100%'}]`;
+  const height = `min-h-[${props.width || '100%'}]`;
   return (
     <motion.div
-      className={
-        'flex flex-col' + ` w-[${props.width}] min-h-[${props.height}]`
-      }
+      className={'flex flex-col' + `${width} ${height}`}
       animate={{ y: '5vh', x: '4vw', scale: 1 }}
       drag
       dragConstraints={{ top: 0, left: 0, right: 0, bottom: 0 }}
@@ -14,7 +14,7 @@ function BasicWindow(props) {
       transition={{ duration: 0.4, ease: 'easeIn', delay: 0.5 }}
     >
       <div
-        className={`order-last flex flex-col w-full min-h-[${props.height}] rounded-b-md bg-slate-800`}
+        className={`order-last flex flex-col w-full ${height} rounded-b-md bg-slate-800`}
       >
         <p></p>
       </div>
