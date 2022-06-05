dab7f5ea9f73fda407d5bbe871dbf271701a178e
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2022-06-06 00:26:59 +0200

    added topbar dropdown

--- components/ui/topbar/topbar-elements.js
+++ components/ui/topbar/topbar-elements.js
@@ -1,21 +1,24 @@
 import { motion } from 'framer-motion';
 import { BiSun, BiMoon, BiVolumeFull, BiVolumeMute } from 'react-icons/bi';
+import Dropdown from './dropdown';
 
 function TopBarElements(props) {
   return (
-    <motion.div className="flex flex-row ml-3 mr-5 justify-between">
-      <div className="flex flex-row gap-3 justify-center pl-3">
-        <button>b1</button>
-        <button>b2</button>
-        <button>b3</button>
-      </div>
-      <div className="flex flex-row gap-4">
-        <button>
-          <BiVolumeFull className="h-6 w-6" />
-        </button>
-        <button>
-          <BiMoon className="h-6 w-6 hover:animate-spin" />
-        </button>
+    <motion.div className="flex flex-col h-max content-center">
+      <div className=" flex flex-row ml-3 mr-5 justify-between">
+        <div className="flex flex-row gap-3 justify-center pl-3">
+          <Dropdown>File</Dropdown>
+          <Dropdown>Options</Dropdown>
+          <Dropdown>Utilities</Dropdown>
+        </div>
+        <div className="flex flex-row gap-4">
+          <button>
+            <BiVolumeFull className="h-6 w-6" />
+          </button>
+          <button>
+            <BiMoon className="h-6 w-6 hover:animate-spin" />
+          </button>
+        </div>
       </div>
     </motion.div>
   );
--- components/ui/topbar/topbar.js
+++ components/ui/topbar/topbar.js
@@ -3,7 +3,7 @@ import TopBarElements from './topbar-elements';
 
 function TopBar(props) {
   return (
-    <motion.div className=" grid-span-12 w-full h-9 bg-slate-400 items-center">
+    <motion.div className="flex flex-col grid-span-12 w-full h-9 bg-slate-400 justify-center">
       <TopBarElements />
     </motion.div>
   );
