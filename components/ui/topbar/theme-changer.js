ae837c20c1b67f0a4ed0d04641748b3a39a4160a
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2022-06-06 12:15:15 +0200

    added cool theme switcher

--- components/ui/topbar/theme-changer.js
+++ components/ui/topbar/theme-changer.js
@@ -1,24 +1,22 @@
 import { BiSun, BiMoon } from 'react-icons/bi';
 import { useTheme } from 'next-themes';
+import { DarkModeSwitch } from 'react-toggle-dark-mode';
 
 function ThemeChanger() {
   const { theme, setTheme } = useTheme();
 
-  if (theme === 'dark') {
-    return (
-      <button onClick={() => setTheme('light')}>
-        <BiSun className="h-6 w-6" />
-      </button>
-    );
-  } else if (theme === 'light') {
-    return (
-      <button onClick={() => setTheme('dark')}>
-        <BiMoon className="h-6 w-6" />
-      </button>
-    );
-  } else {
-    return null;
-  }
+  const toggleDarkMode = (checked) => {
+    checked ? setTheme('light') : setTheme('dark');
+  };
+
+  return (
+    <DarkModeSwitch
+      style={{ marginTop: '.6vh' }}
+      checked={theme === 'light' ? true : false}
+      onChange={toggleDarkMode}
+      size={25}
+    />
+  );
 }
 
 export default ThemeChanger;
