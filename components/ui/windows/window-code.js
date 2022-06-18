f0b77a08e1388ae5604964fbacf368dd989a4830
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2022-06-18 03:09:14 +0200

    added terminal window

--- components/ui/windows/window-code.js
+++ components/ui/windows/window-code.js
@@ -1,23 +1,38 @@
-import { motion } from 'framer-motion';
-import CodeAutoTyping from 'react-code-auto-typing';
-import { monokai } from 'react-syntax-highlighter/dist/cjs/styles/hljs';
 import { Fragment } from 'react/cjs/react.production.min';
+import { ReactTerminal } from 'react-terminal';
+import { useRouter } from 'next/router';
 
 function WindowCode() {
-  const snippet = `function greet(name) {
-    return 'Hello, my name is ' + name;
-  };
-  
-  console.log(greet('Andrea'))`;
+  const router = useRouter();
 
+  // Define commands here
+  const commands = {
+    whoami: 'Andrea Poltronieri',
+    whatdoido: 'I am a Ph.D. in Computer Science and Engineering',
+    cd: (directory) => `changed path to ${directory}`,
+    navigate: (page) => {
+      if (page in ['home', 'projects', 'resume', 'pubblications', 'contact']) {
+        router.push(`/${page}`);
+      } else {
+        ('Page not found. Try with a valid page.');
+      }
+    },
+  };
+
+  // TODO: fix
   return (
-    <CodeAutoTyping
-      className="min-h-full"
-      language="bash"
-      syntaxHighlighterProps={{ style: monokai }}
-    >
-      {snippet}
-    </CodeAutoTyping>
+    <div className=" h-96 ">
+      {' '}
+      <ReactTerminal
+        prompt="andreapoltronieri@website - %"
+        welcomeMessage={`Welcome to my website!
+        You can navigate it using the terminal or `}
+        commands={commands}
+        showControlBar={false}
+        showControlButtons={false}
+        theme={'dark'}
+      />
+    </div>
   );
 }
 
