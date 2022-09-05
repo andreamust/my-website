d7a0e046e5cd6a4eb50108a0fa39b2e8cddb3cbe
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2022-09-06 00:50:24 +0200

    arrow bug fixing

--- components/page-components/resume/resume-timebar.js
+++ components/page-components/resume/resume-timebar.js
@@ -7,27 +7,24 @@ function ResumeTimebar(props) {
   const years = props.data;
 
   return (
-    <Xwrapper>
-      <div className="sticky flex-none h-full basis-2 flex flex-col justify-evenly">
-        {years.map((year, index) => {
-          return (
-            <Fragment key={year}>
-              <div
-                id={`year-${index}`}
-                className={`flex-1 w-12 max-h-12 text-center rounded-full ${
-                  props.year === year ? ' bg-red-300' : ' bg-green-400'
-                }`}
-              >
-                <button onClick={() => props.yearHandler(year)}>
-                  <p className="h-12 text-center">{year}</p>
-                </button>
-              </div>
-            </Fragment>
-          );
-        })}
-      </div>
-      <Xarrow start={'comp1'} end={'comp2'} />
-    </Xwrapper>
+    <div className="sticky flex-none h-full basis-2 flex flex-col justify-evenly">
+      {years.map((year, index) => {
+        return (
+          <Fragment key={year}>
+            <div
+              id={`year-${index}`}
+              className={`flex-1 w-12 max-h-12 text-center rounded-full ${
+                props.year === year ? ' bg-red-300' : ' bg-green-400'
+              }`}
+            >
+              <button onClick={() => props.yearHandler(year)}>
+                <p className="h-12 text-center">{year}</p>
+              </button>
+            </div>
+          </Fragment>
+        );
+      })}
+    </div>
   );
 }
 
