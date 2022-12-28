346ac07dc1ef2ba0b0318aa858d83172f0357413
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2022-12-28 15:41:35 +0100

    removed deploy error

--- components/page-components/pubblications/pubblications-layout.js
+++ components/page-components/pubblications/pubblications-layout.js
@@ -2,22 +2,7 @@ import Xarrow, { useXarrow, Xwrapper } from 'react-xarrows';
 
 function PubblicationsLayout(props) {
   const updateXarrow = useXarrow();
-  return (
-    <div>
-      <Xwrapper>
-        <div id="comp1" className="bg-red-300 w-12 h-12 ml-8"></div>
-        <div id="comp2" className="bg-green-300 w-12 h-12 ml-80"></div>
-        <Xarrow
-          start={'comp1'}
-          end={'comp2'}
-          color={'black'}
-          curveness={0}
-          showHead={false}
-          strokeWidth={2}
-        />
-      </Xwrapper>
-    </div>
-  );
+  return <div>More content</div>;
 }
 
 export default PubblicationsLayout;
--- components/page-components/resume/resume-content.js
+++ components/page-components/resume/resume-content.js
@@ -28,7 +28,7 @@ function ResumeContent(props) {
               <h3 className=" text-lg font-modernMono">{company}</h3>
             </div>
             <p className="pt-3">
-              <ReactMarkdown children={content} remarkPlugins={[remarkGfm]} />
+              <ReactMarkdown children={content} />
             </p>
           </div>
         );
