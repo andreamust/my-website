ea9abe56adfb17ed5083ee7161d3f4fc8f6ad753
andreamust <andrea.poltronieri21@gmail.com>
2023-03-13 18:17:24 +0100

    projects layout

---
+++ components/page-components/projects/projects-layout.js
@@ -0,0 +1,11 @@
+import ProjectsContent from './projects-content';
+
+function ProjectsLayout(props) {
+  return (
+    <div className="flex flex-col w-screen h-screen items-center pt-24 pb-60 overflow-scroll">
+      <ProjectsContent pubblications={props.pubblications} />
+    </div>
+  );
+}
+
+export default ProjectsLayout;
--- pages/projects/index.js
+++ pages/projects/index.js
@@ -1,14 +1,31 @@
+import ProjectsLayout from '../../components/page-components/projects/projects-layout';
 import BasicWindow from '../../components/ui/windows/basic-window';
+import fs from 'fs/promises';
+import path from 'path';
 
 function Projects(props) {
+  const projects = props;
   return (
     <BasicWindow
       title={'andrea-poltronieri/projects'}
       width={'50vw'}
       height={'70vh'}
       animate={false}
-    ></BasicWindow>
+    >
+      <div className="bg-inherit bg-slate-400 bg-opacity-30">
+        <h1 className="font-modernMono text-4xl leading-[3.8rem] text-left pl-9 pt-3 mb-3 bg-opacity-30">
+          Projects
+        </h1>
+      </div>
+      <ProjectsLayout projects={projects} />
+    </BasicWindow>
   );
+}
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'data', 'projects.json');
+  const pubblicationData = await fs.readFile(filePath);
+  const data = JSON.parse(pubblicationData);
+  return { props: { records: projects } };
 }
 
 export default Projects;
