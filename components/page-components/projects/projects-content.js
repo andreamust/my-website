55fc8d36184c868a5814efbd145ae7e44e341db5
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2023-03-24 13:15:54 +0100

    finish styling projects

--- components/page-components/projects/projects-content.js
+++ components/page-components/projects/projects-content.js
@@ -12,7 +12,7 @@ function ProjectsContent(props) {
           key={project.short_title}
           className="flex flex-col border-gray-900 w-10/12 h-full border-2"
         >
-          <div className="flex flex-row items-center  p-4">
+          <div className="flex flex-row items-center p-4">
             <Image
               className="flex-1"
               src={project.image}
@@ -22,8 +22,10 @@ function ProjectsContent(props) {
             />
             <div className="flex flex-col items-center justify-between w-full h-full">
               <div className="flex flex-row items-center justify-between p-4 w-full">
-                <h1 className="text-3xl font-modernBold text-gray-800 text-center">
-                  {project.short_title}
+                <h1 className="text-3xl font-modernBold text-gray-800 text-center pl-10">
+                  <Link href={'/projects/?project=' + project.short_title}>
+                    <a>{project.short_title}</a>
+                  </Link>
                 </h1>
                 <h2 className="text-xl font-modernMono text-gray-800 text-right">
                   {project.title}
@@ -33,18 +35,13 @@ function ProjectsContent(props) {
                 <h3 className="flex-1 basis-1/3 text-xl font-modernMono text-gray-800 text-center">
                   {project.year}
                 </h3>
-                <h4 className="flex-1 basis-1/3 text-xl font-modernMono text-gray-800 text-center">
+                <h4 className="flex-1 basis-1/3 text-lg font-modernMono text-gray-800 text-center">
                   {project.type}
                 </h4>
-                <div className="flex-1 basis-1/3 ">
+                <div className="flex-1 basis-1/3 h-full">
                   <Link href={project.link}>
-                    <a className="flex flex-row gap-2 ">
-                      <div className="w-6 align-middle flex-1 items-end">
-                        <BsGithub className="w-8" />
-                      </div>
-                      <p className="flex-1 text-md font-modernMono text-gray-800 text-right">
-                        Repository
-                      </p>
+                    <a className="flex flex-col">
+                      <BsGithub className="self-center w-7 h-7" />
                     </a>
                   </Link>
                 </div>
