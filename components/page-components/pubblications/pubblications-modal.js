6476763895374689accd12b4848de682e7dfc56c
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2023-12-07 18:10:03 +0100

    add copy to pubblication modal

--- components/page-components/pubblications/pubblications-modal.js
+++ components/page-components/pubblications/pubblications-modal.js
@@ -3,6 +3,18 @@ import renderCitation from '../../utils/process-pubblications';
 import { useState } from 'react';
 
 function PubblicationsModal(props) {
+  const [copySuccess, setCopySuccess] = useState('');
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(citation).then(
+      () => {
+        setCopySuccess('Copied!');
+      },
+      () => {
+        setCopySuccess('Failed to copy!');
+      }
+    );
+  };
   const { content, router } = props;
   const authorList = content.author.map(
     (author) => author.family + ' ' + author.given
@@ -49,37 +61,49 @@ function PubblicationsModal(props) {
         <p className="text-md font-modern">{content.abstract}</p>
         <h3 className="text-xl font-modernBold">Link</h3>
         <button
-          className="text-lg bg-purple-600 hover:bg-purple-800 active:bg-green-400 hover:text-white font-bold py-2 px-4 rounded"
+          className="text-lg bg-cerise active:bg-lime text-whitepalette active:text-blackpalette-900 font-bold py-2 px-4 rounded"
           onClick={() => window.open(content.URL)}
         >
           Go to pubblication
         </button>
         <h3 className="text-xl font-modernBold">Cite</h3>
-        <div className="flex flex-row gap-7 items-center">
-          <button
-            className="text-lg bg-purple-600 hover:bg-purple-800 active:bg-green-400 hover:text-white font-bold py-2 px-4 rounded"
-            onClick={() => setCitation(generateCitation('bibliography'))}
-          >
-            APA
-          </button>
-          <button
-            className="text-lg bg-purple-600 hover:bg-purple-800 active:bg-green-400 hover:text-white font-bold py-2 px-4 rounded"
-            onClick={() => setCitation(generateCitation('bibtex'))}
-          >
-            BibTex
-          </button>
-          <button
-            className="text-lg bg-purple-600 hover:bg-purple-800 active:bg-green-400 hover:text-white font-bold py-2 px-4 rounded"
-            onClick={() => setCitation(generateCitation('ris'))}
-          >
-            RIS
-          </button>
-          <button
-            className="text-lg bg-purple-600 hover:bg-purple-800 active:bg-green-400 hover:text-white font-bold py-2 px-4 rounded"
-            onClick={() => setCitation(generateCitation('ndjson'))}
-          >
-            NDJSON
-          </button>
+        <div className="flex justify-between items-center">
+          <div className="flex flex-row gap-4 items-center">
+            <button
+              className="text-lg bg-lime hover:bg-purple-800 active:bg-green-400 font-bold py-2 px-4 rounded"
+              onClick={() => setCitation(generateCitation('bibliography'))}
+            >
+              APA
+            </button>
+            <button
+              className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette font-bold py-2 px-4 rounded"
+              onClick={() => setCitation(generateCitation('bibtex'))}
+            >
+              BibTex
+            </button>
+            <button
+              className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette font-bold py-2 px-4 rounded"
+              onClick={() => setCitation(generateCitation('ris'))}
+            >
+              RIS
+            </button>
+            <button
+              className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette font-bold py-2 px-4 rounded"
+              onClick={() => setCitation(generateCitation('ndjson'))}
+            >
+              NDJSON
+            </button>
+          </div>
+          <div className="flex justify-between items-center">
+            {citation && (
+              <button
+                className="text-lg bg-greypalette-600 hover:text-cerise font-bold py-2 px-4 rounded ml-50"
+                onClick={copyToClipboard}
+              >
+                {copySuccess ? copySuccess : 'Copy'}
+              </button>
+            )}
+          </div>
         </div>
         <p className="text-md font-modern">{citation}</p>
       </div>
