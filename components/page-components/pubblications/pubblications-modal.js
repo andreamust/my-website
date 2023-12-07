3c5b9368d026815c329f16c76f4bf9a2139b40cd
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2023-12-07 21:37:00 +0100

    refined dark stile pubblications

--- components/page-components/pubblications/pubblications-modal.js
+++ components/page-components/pubblications/pubblications-modal.js
@@ -1,9 +1,12 @@
 import BasicWindow from '../../ui/windows/basic-window';
 import renderCitation from '../../utils/process-pubblications';
 import { useState } from 'react';
+import useMobile from '../../utils/mobile';
 
 function PubblicationsModal(props) {
+  const mobile = useMobile();
   const [copySuccess, setCopySuccess] = useState('');
+  console.log(mobile);
 
   const copyToClipboard = () => {
     navigator.clipboard.writeText(citation).then(
@@ -32,7 +35,7 @@ function PubblicationsModal(props) {
     <BasicWindow
       title={content.id}
       width="w-[70%]"
-      height="h-[60%]"
+      height="h-[70%]"
       animate={true}
       closePath="/pubblications/"
       onClose={() => {
@@ -40,11 +43,15 @@ function PubblicationsModal(props) {
       }}
     >
       <div className="flex flex-col gap-5 w-full p-12 overflow-scroll scroll-smooth no-scrollbar">
-        <h1 className="text-2xl font-modernBold">{content.title}</h1>
+        <h1 className="text-2xl font-modernBold dark:text-blackpalette-900 underline decoration-wavy decoration-lime decoration-4 underline-offset-4">
+          {content.title}
+        </h1>
         <h3 className="text-xl font-modernBold">Authors</h3>
-        <p className="text-md font-modern">{authorList.join(', ')}</p>
+        <p className="text-md font-modern dark:text-blackpalette-900">
+          {authorList.join(', ')}
+        </p>
         <h3 className="text-xl font-modernBold">Published in</h3>
-        <p className="text-md font-modern italic">
+        <p className="text-md font-modern italic dark:text-blackpalette-900">
           {content['container-title']
             ? content['container-title']
             : 'To be published'}
@@ -54,11 +61,15 @@ function PubblicationsModal(props) {
         ) : (
           <>
             <h3 className="text-xl font-modernBold">Note</h3>
-            <p className="text-md font-modern">Alphabetical order</p>
+            <p className="text-md font-modern dark:text-blackpalette-900">
+              Alphabetical order
+            </p>
           </>
         )}
         <h3 className="text-xl font-modernBold">Abstract</h3>
-        <p className="text-md font-modern">{content.abstract}</p>
+        <p className="text-md font-modern dark:text-blackpalette-900">
+          {content.abstract}
+        </p>
         <h3 className="text-xl font-modernBold">Link</h3>
         <button
           className="text-lg bg-cerise active:bg-lime text-whitepalette active:text-blackpalette-900 font-bold py-2 px-4 rounded"
@@ -70,25 +81,25 @@ function PubblicationsModal(props) {
         <div className="flex justify-between items-center">
           <div className="flex flex-row gap-4 items-center">
             <button
-              className="text-lg bg-lime hover:bg-cerise active:bg-green-400 font-bold py-2 px-4 rounded"
+              className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette dark:text-blackpalette-900 font-bold py-2 px-4 rounded"
               onClick={() => setCitation(generateCitation('bibliography'))}
             >
               APA
             </button>
             <button
-              className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette font-bold py-2 px-4 rounded"
+              className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette dark:text-blackpalette-900 font-bold py-2 px-4 rounded"
               onClick={() => setCitation(generateCitation('bibtex'))}
             >
               BibTex
             </button>
             <button
-              className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette font-bold py-2 px-4 rounded"
+              className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette dark:text-blackpalette-900 font-bold py-2 px-4 rounded"
               onClick={() => setCitation(generateCitation('ris'))}
             >
               RIS
             </button>
             <button
-              className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette font-bold py-2 px-4 rounded"
+              className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette dark:text-blackpalette-900 font-bold py-2 px-4 rounded"
               onClick={() => setCitation(generateCitation('ndjson'))}
             >
               NDJSON
@@ -97,7 +108,7 @@ function PubblicationsModal(props) {
           <div className="flex justify-between items-center">
             {citation && (
               <button
-                className="text-lg bg-greypalette-600 hover:text-cerise font-bold py-2 px-4 rounded ml-50"
+                className="text-lg bg-greypalette-600 dark:bg-whitepalette hover:text-cerise dark:text-blackpalette-900 font-bold py-2 px-4 rounded ml-50"
                 onClick={copyToClipboard}
               >
                 {copySuccess ? copySuccess : 'Copy'}
@@ -105,7 +116,9 @@ function PubblicationsModal(props) {
             )}
           </div>
         </div>
-        <p className="text-md font-modern">{citation}</p>
+        <p className="text-md font-modern dark:text-blackpalette-900">
+          {citation}
+        </p>
       </div>
     </BasicWindow>
   );
