a567acfc3e4e9d0c627c487228901739088464fe
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2023-12-07 14:32:27 +0100

    first edit to global style

--- components/page-components/home/home-bio-content.js
+++ components/page-components/home/home-bio-content.js
@@ -45,15 +45,8 @@ const bios = {
       transition={{ duration: 0.4 }}
     >
       I&apos;m a PhD Student in Computer Science and Engineering at the
-      University of Bologna. I&apos;m currently working on the{' '}
-      <Link href="https://polifonia-project.eu/">
-        <a target={'_blank'}>Polifonia Project</a>
-      </Link>
-      . I&apos;m also part of the{' '}
-      <Link href="http://stlab.istc.cnr.it/stlab/">
-        <a target={'_blank'}>Semantic Web Laboratory (STLab)</a>
-      </Link>{' '}
-      of the Italian National Council for Research (CNR)
+      University of Bologna. My main reasearch interest is about Music
+      Information Retrieval (MIR) and Computational Musicology.
     </motion.p>
   ),
   4: (
@@ -64,17 +57,14 @@ const bios = {
       transition={{ duration: 0.4 }}
     >
       I&apos;m a PhD Student in Computer Science and Engineering at the
-      University of Bologna. I&apos;m currently working on the{' '}
+      University of Bologna. My main reasearch interest is about Music
+      Information Retrieval (MIR) and Computational Musicology. More
+      specifically, I&apos;m interested in the multi-modal analysis of harmonic
+      data. I&apos;m currently working on the{' '}
       <Link href="https://polifonia-project.eu/">
         <a target={'_blank'}>Polifonia Project</a>
       </Link>
-      . I&apos;m also part of the{' '}
-      <Link href="http://stlab.istc.cnr.it/stlab/">
-        <a target={'_blank'}>Semantic Web Laboratory (STLab)</a>
-      </Link>{' '}
-      of the Italian National Council for Research (CNR). My main reasearch
-      interest is about Music Information Retrieval (MIR) and Computational
-      Musicology.
+      .
     </motion.p>
   ),
 };
