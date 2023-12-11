f74a39b63d924fa9f3faadcee0c7fc941e6c09d9
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2023-12-12 00:18:31 +0100

    delete manifest.json references

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,6 @@ function Home() {
         <meta name="twitter:site" content="@andreapoltronieri" />
         <meta name="twitter:creator" content="@andreapoltronieri" />
         <link rel="apple-touch-icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json" />
         <meta name="theme-color" content="#000000" />
       </Head>
       <HomeHero>
