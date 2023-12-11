48af17d557880e2dd5517280f620ce50cea8ced9
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2023-12-11 23:59:59 +0100

    fix projects modal

--- components/page-components/projects/projects-modal.js
+++ components/page-components/projects/projects-modal.js
@@ -1,15 +1,18 @@
 import BasicWindow from '../../ui/windows/basic-window';
 import Image from 'next/image';
 import Link from 'next/link';
+import { FaGithub } from 'react-icons/fa';
+import { SiZenodo } from 'react-icons/si';
+import { IoIosDocument } from 'react-icons/io';
 
 function ProjectsModal(props) {
   const { content, router } = props;
 
   return (
     <BasicWindow
-      title={content.short_title}
-      width="w-[70%]"
-      height="h-[60%]"
+      title={content.short_title.replace('_', '.')}
+      width="w-[80%]"
+      height="h-[70%]"
       animate={true}
       closePath="/projects/"
       onClose={() => {
@@ -17,20 +20,39 @@ function ProjectsModal(props) {
       }}
     >
       <div className="flex flex-col gap-5 w-full p-12 overflow-scroll scroll-smooth no-scrollbar">
-        <h1 className="text-2xl font-modernBold">{content.title}</h1>
-        <div className="w-full h-full items-center">
-          <Image src={content.image} width={400} height={400} quality={100} />
-        </div>
+        <h1 className="text-2xl font-modernBold underline decoration-wavy decoration-lime decoration-2">
+          {content.title}
+        </h1>
         <h3 className="text-xl font-modernBold">Year</h3>
         <p className="text-md font-modern">{content.year}</p>
         <h3 className="text-xl font-modernBold">Descritpion</h3>
         <p className="text-md font-modern">{content.description}</p>
-        <h3 className="text-xl font-modernBold">Repository</h3>
-        <Link href={content.link}>
-          <p className="text-md font-modernMono">
-            <a>{content.link}</a>
-          </p>
-        </Link>
+        <h3 className="text-xl font-modernBold">Links</h3>
+        <div className="flex flex-row gap-5">
+          {content.link && (
+            <p className="text-md font-modern">
+              <Link href={content.link}>
+                <a target="_blank" aria-label="Link to project">
+                  <FaGithub className="h-10 w-10 drop-shadow-lg dark:text-lime text-cerise" />
+                </a>
+              </Link>
+            </p>
+          )}
+          {content.paper && (
+            <Link href={content.paper}>
+              <a target="_blank" aria-label="Link to paper">
+                <IoIosDocument className="h-10 w-10 drop-shadow-lg dark:text-lime text-cerise" />
+              </a>
+            </Link>
+          )}
+          {content.zenodo && (
+            <Link href={content.zenodo}>
+              <a target="_blank" aria-label="Link to data">
+                <SiZenodo className="h-12 w-12 drop-shadow-lg dark:text-lime text-cerise" />
+              </a>
+            </Link>
+          )}
+        </div>
       </div>
     </BasicWindow>
   );
--- components/page-components/pubblications/pubblications-modal.js
+++ components/page-components/pubblications/pubblications-modal.js
@@ -2,11 +2,11 @@ import BasicWindow from '../../ui/windows/basic-window';
 import renderCitation from '../../utils/process-pubblications';
 import { useState } from 'react';
 import useMobile from '../../utils/mobile';
+import Head from 'next/head';
 
 function PubblicationsModal(props) {
   const mobile = useMobile();
   const [copySuccess, setCopySuccess] = useState('');
-  console.log(mobile);
 
   const copyToClipboard = () => {
     navigator.clipboard.writeText(citation).then(
@@ -32,95 +32,126 @@ function PubblicationsModal(props) {
   let [citation, setCitation] = useState();
 
   return (
-    <BasicWindow
-      title={content.id}
-      width="w-[80%]"
-      height="h-[70%]"
-      animate={true}
-      closePath="/pubblications/"
-      onClose={() => {
-        router.push('/pubblications/');
-      }}
-    >
-      <div className="flex flex-col gap-5 w-full p-12 overflow-scroll scroll-smooth no-scrollbar">
-        <h1 className="text-2xl font-modernBold dark:text-blackpalette-900 underline decoration-wavy decoration-lime decoration-4 underline-offset-4">
-          {content.title}
-        </h1>
-        <h3 className="text-xl font-modernBold">Authors</h3>
-        <p className="text-md font-modern dark:text-blackpalette-900">
-          {authorList.join(', ')}
-        </p>
-        <h3 className="text-xl font-modernBold">Published in</h3>
-        <p className="text-md font-modern italic dark:text-blackpalette-900">
-          {content['container-title']
-            ? content['container-title']
-            : 'To be published'}
-        </p>
-        {authorList[0] == 'Poltronieri Andrea' ? (
-          ''
-        ) : (
-          <>
-            <h3 className="text-xl font-modernBold">Note</h3>
-            <p className="text-md font-modern dark:text-blackpalette-900">
-              Alphabetical order
-            </p>
-          </>
-        )}
-        <h3 className="text-xl font-modernBold">Abstract</h3>
-        <p className="text-md font-modern dark:text-blackpalette-900">
-          {content.abstract}
-        </p>
-        <h3 className="text-xl font-modernBold">Link</h3>
-        <button
-          className="text-lg bg-cerise active:bg-lime text-whitepalette active:text-blackpalette-900 font-bold py-2 px-4 rounded"
-          onClick={() => window.open(content.URL)}
-        >
-          Go to pubblication
-        </button>
-        <h3 className="text-xl font-modernBold">Cite</h3>
-        <div className="flex justify-between items-center">
-          <div className="flex flex-row gap-4 items-center">
-            <button
-              className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette dark:text-blackpalette-900 font-bold py-2 px-4 rounded"
-              onClick={() => setCitation(generateCitation('bibliography'))}
-            >
-              APA
-            </button>
-            <button
-              className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette dark:text-blackpalette-900 font-bold py-2 px-4 rounded"
-              onClick={() => setCitation(generateCitation('bibtex'))}
-            >
-              BibTex
-            </button>
-            <button
-              className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette dark:text-blackpalette-900 font-bold py-2 px-4 rounded"
-              onClick={() => setCitation(generateCitation('ris'))}
-            >
-              RIS
-            </button>
-            <button
-              className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette dark:text-blackpalette-900 font-bold py-2 px-4 rounded"
-              onClick={() => setCitation(generateCitation('ndjson'))}
-            >
-              NDJSON
-            </button>
-          </div>
+    (
+      <Head>
+        <title>{content.id} | Andrea Poltronieri</title>
+        <meta
+          name="description"
+          content="Andrea Poltronieri's personal website - Pubblications"
+        />
+        <meta
+          name="keywords"
+          content="andrea, poltronieri, andrea poltronieri, poltronieri andrea, andrea poltronieri website, poltronieri andrea website, andrea poltronieri personal website, poltronieri andrea personal website, andrea poltronieri personal website, poltronieri andrea personal websi, researcher, unibo, university of bologna, bologna, italy, computer science, computer engineering, software engineering, software developer, software, machine learning, artificial intelligence, ai, ml, data science, data, science, data engineer, data engineering, data scientist, data science, data analytics, data analyst, data analysis, data visualization, data visualisation, data visualizer, semantic web, ontology, linked open data, linked data, rdf, owl, owl2, owl2rl, owl2, music, music information retrieval, music information, music technology, music, computational musicoogy, computational art"
+        ></meta>
+        <meta name="author" content="Andrea Poltronieri" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#000000" />
+        <link rel="icon" href="images/favicon.ico" />
+        <meta name="citation_title" content={content.title} />
+        <meta name="citation_author" content={authorList.join(', ')} />
+        <meta name="citation_publication_date" content={content.year} />
+        <meta
+          name="citation_journal_title"
+          content={content['container-title']}
+        />
+        <meta name="citation_abstract" content={content.abstract} />
+        <meta name="citation_pdf_url" content={content.URL} />
+        <meta name="citation_download_url" content={content.URL} />
+        <meta name="citation_public_url" content={content.URL} />
+        <meta name="citation_volume" content={content.volume} />
+      </Head>
+    ),
+    (
+      <BasicWindow
+        title={content.id}
+        width="w-[80%]"
+        height="h-[70%]"
+        animate={true}
+        closePath="/pubblications/"
+        onClose={() => {
+          router.push('/pubblications/');
+        }}
+      >
+        <div className="flex flex-col gap-5 w-full p-12 overflow-scroll scroll-smooth no-scrollbar">
+          <h1 className="text-2xl font-modernBold dark:text-blackpalette-900 underline decoration-wavy decoration-lime decoration-4 underline-offset-4">
+            {content.title}
+          </h1>
+          <h3 className="text-xl font-modernBold">Authors</h3>
+          <p className="text-md font-modern dark:text-blackpalette-900">
+            {authorList.join(', ')}
+          </p>
+          <h3 className="text-xl font-modernBold">Published in</h3>
+          <p className="text-md font-modern italic dark:text-blackpalette-900">
+            {content['container-title']
+              ? content['container-title']
+              : 'To be published'}
+          </p>
+          {authorList[0] == 'Poltronieri Andrea' ? (
+            ''
+          ) : (
+            <>
+              <h3 className="text-xl font-modernBold">Note</h3>
+              <p className="text-md font-modern dark:text-blackpalette-900">
+                Alphabetical order
+              </p>
+            </>
+          )}
+          <h3 className="text-xl font-modernBold">Abstract</h3>
+          <p className="text-md font-modern dark:text-blackpalette-900">
+            {content.abstract}
+          </p>
+          <h3 className="text-xl font-modernBold">Link</h3>
+          <button
+            className="text-lg bg-cerise active:bg-lime text-whitepalette active:text-blackpalette-900 font-bold py-2 px-4 rounded"
+            onClick={() => window.open(content.URL)}
+          >
+            Go to pubblication
+          </button>
+          <h3 className="text-xl font-modernBold">Cite</h3>
           <div className="flex justify-between items-center">
-            {citation && (
+            <div className="flex flex-row gap-4 items-center">
               <button
-                className="text-lg bg-greypalette-600 dark:bg-whitepalette hover:text-cerise dark:text-blackpalette-900 font-bold py-2 px-4 rounded ml-50"
-                onClick={copyToClipboard}
+                className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette dark:text-blackpalette-900 font-bold py-2 px-4 rounded"
+                onClick={() => setCitation(generateCitation('bibliography'))}
               >
-                {copySuccess ? copySuccess : 'Copy'}
+                APA
               </button>
-            )}
+              <button
+                className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette dark:text-blackpalette-900 font-bold py-2 px-4 rounded"
+                onClick={() => setCitation(generateCitation('bibtex'))}
+              >
+                BibTex
+              </button>
+              <button
+                className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette dark:text-blackpalette-900 font-bold py-2 px-4 rounded"
+                onClick={() => setCitation(generateCitation('ris'))}
+              >
+                RIS
+              </button>
+              <button
+                className="text-lg bg-lime hover:bg-cerise hover:text-whitepalette dark:text-blackpalette-900 font-bold py-2 px-4 rounded"
+                onClick={() => setCitation(generateCitation('ndjson'))}
+              >
+                NDJSON
+              </button>
+            </div>
+            <div className="flex justify-between items-center">
+              {citation && (
+                <button
+                  className="text-lg bg-greypalette-600 dark:bg-whitepalette hover:text-cerise dark:text-blackpalette-900 font-bold py-2 px-4 rounded ml-50"
+                  onClick={copyToClipboard}
+                >
+                  {copySuccess ? copySuccess : 'Copy'}
+                </button>
+              )}
+            </div>
           </div>
+          <p className="text-md font-modern dark:text-blackpalette-900">
+            {citation}
+          </p>
         </div>
-        <p className="text-md font-modern dark:text-blackpalette-900">
-          {citation}
-        </p>
-      </div>
-    </BasicWindow>
+      </BasicWindow>
+    )
   );
 }
 
