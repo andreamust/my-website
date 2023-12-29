b11eba6d52b3948378c60faaa8ccb248b76b2318
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2023-12-29 14:28:41 +0100

    update SEO home page

--- pages/_app.js
+++ pages/_app.js
@@ -8,7 +8,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="system" attribute="class">
       <Head>
-        <title>Andrea Poltronieri</title>
+        <title>Andrea Poltronieri's Website</title>
         <meta
           name="description"
           content="Andrea Poltronieri's personal website"
@@ -17,10 +17,24 @@ function MyApp({ Component, pageProps }) {
           name="keywords"
           content="andrea, poltronieri, andrea poltronieri, poltronieri andrea, andrea poltronieri website, poltronieri andrea website, andrea poltronieri personal website, poltronieri andrea personal website, andrea poltronieri personal website, poltronieri andrea personal websi, researcher, unibo, university of bologna, bologna, italy, computer science, computer engineering, software engineering, software developer, software, machine learning, artificial intelligence, ai, ml, data science, data, science, data engineer, data engineering, data scientist, data science, data analytics, data analyst, data analysis, data visualization, data visualisation, data visualizer, semantic web, ontology, linked open data, linked data, rdf, owl, owl2, owl2rl, owl2, music, music information retrieval, music information, music technology, music, computational musicoogy, computational art"
         ></meta>
+        <meta name="robots" content="index, follow" />
+        <meta name="language" content="English" />
+        <meta name="revisit-after" content="7 days" />
         <meta name="author" content="Andrea Poltronieri" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="theme-color" content="#000000" />
         <link rel="icon" href="images/favicon.ico" />
+        <link rel="apple-touch-icon" href="images/favicon.ico" />
+        <meta property="og:title" content="Andrea Poltronieri" />
+        <meta
+          property="og:description"
+          content="Andrea Poltronieri's personal website"
+        />
+        <meta property="og:image" content="images/favicon.ico" />
+        <meta property="og:url" content="https://andreapoltronieri.com" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@andpoltronieri" />
+        <meta name="twitter:creator" content="@andpoltronieri" />
       </Head>
       <Layout>
         <Component {...pageProps} />
