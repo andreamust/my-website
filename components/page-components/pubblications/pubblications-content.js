d8808c17ca0ffbf8ece7e99b7e24534266f38420
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2024-11-26 18:51:03 +0100

    update pubblications layout

--- components/page-components/pubblications/pubblications-content.js
+++ components/page-components/pubblications/pubblications-content.js
@@ -1,9 +1,9 @@
-import { MdZoomOutMap } from 'react-icons/md';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import PubblicationsModal from './pubblications-modal';
-import { Fragment } from 'react';
-import renderCitation from '../../utils/process-pubblications';
+import { MdZoomOutMap } from "react-icons/md";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import PubblicationsModal from "./pubblications-modal";
+import { Fragment } from "react";
+import renderCitation from "../../utils/process-pubblications";
 
 function PubblicationsContent(props) {
   const pubblications = props.pubblications.records;
@@ -19,26 +19,25 @@ function PubblicationsContent(props) {
   const loadedYears = [];
 
   return (
-    <div className="flex flex-col items-center gap-10 w-10/12 md:w-7/12">
+    <div className="flex flex-col items-start gap-10 w-10/12 md:w-7/12">
       {[...pubblications].reverse().map((pubblication) => {
-        let { output, data } = renderCitation(pubblication, 'bibliography');
-        const year = data.issued['date-parts'][0][0];
+        let { output, data } = renderCitation(pubblication, "bibliography");
+        const year = data.issued["date-parts"][0][0];
         const title = data.title;
-        const url = data['URL'];
         // string pre-processing
-        const splitCitation = output.split('Poltronieri, A.');
+        const splitCitation = output.split("Poltronieri, A.");
         const splitTitle = splitCitation[1].split(title);
         return (
           <Fragment key={data.id}>
             {loadedYears.includes(year)
-              ? ''
+              ? ""
               : loadedYears.push(year) && (
-                  <h2 className="text-2xl font-bold circle-sketch-highlight">
+                  <h2 className="text-2xl self-center font-bold circle-sketch-highlight">
                     {year}
                   </h2>
                 )}
             <div className="flex flex-row gap-7 items-center">
-              <Link href={'/pubblications/?pubblication=' + data.id}>
+              <Link href={"/pubblications/?pubblication=" + data.id}>
                 <a>
                   <MdZoomOutMap
                     //zoom on hover and animate on click
@@ -49,7 +48,7 @@ function PubblicationsContent(props) {
               <p className="font-modern">
                 {splitCitation[0]}
                 <span className="font-modernBold text-blackpalette-900 dark:text-whitepalette underline decoration-wavy decoration-lime decoration-2">
-                  {' '}
+                  {" "}
                   Poltronieri, Andrea
                 </span>
                 {splitTitle[0]}
