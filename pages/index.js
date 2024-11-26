e10225284dcad5097a26edc38d9752b1464e2a97
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2024-11-26 19:03:33 +0100

    remove unused imports

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,7 @@
-import BasicWindow from '../components/ui/windows/basic-window';
-import HomeHero from '../components/page-components/home/home-hero';
-import HomeBio from '../components/page-components/home/home-bio';
-import Head from 'next/head';
-import { Fragment } from 'react/cjs/react.production.min';
+import HomeHero from "../components/page-components/home/home-hero";
+import HomeBio from "../components/page-components/home/home-bio";
+import Head from "next/head";
+import { Fragment } from "react/cjs/react.production.min";
 
 function Home() {
   return (
