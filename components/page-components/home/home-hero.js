b8ca9200f336cae12d59dd986eb60a0a96d513bc
Andrea Poltronieri <andrea.poltronieri21@gmail.com>
2026-04-13 18:49:54 +0200

    improve style

--- components/page-components/home/home-hero.js
+++ components/page-components/home/home-hero.js
@@ -3,9 +3,9 @@ import HomeIcons from "./home-icons";
 
 function HomeHero(props) {
   return (
-    <div className="flex flex-col w-screen h-full gap-7 items-center justify-center pb-28 sm:pb-0">
-      <div className="flex flex-col md:flex-row items-center gap-2 pb-5 w-8/12 md:w-9/12 lg:w-7/12 xl:w-6/12 2xl:w-5/12">
-        <div className="flex flex-col flex-initial w-1/2 items-center h-full pt-12">
+    <div className="flex flex-col w-screen h-full gap-7 items-center justify-center pb-28 md:pb-16 lg:pb-16 md:pt-16 lg:pt-24">
+      <div className="flex flex-col md:flex-row items-center gap-8 md:gap-4 pb-5 w-8/12 md:w-9/12 lg:w-7/12 xl:w-6/12 2xl:w-5/12">
+        <div className="flex flex-col flex-initial w-1/2 items-center pt-10 md:pt-0">
           <div className="h-64 w-64 relative rounded-full overflow-hidden shadow-lg">
             <Image
               src="/images/ap.jpeg"
